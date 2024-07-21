1d500defc59ec462aa366348f4e472f0028733e9
Adarsh Jha <adarsh2005j@gmail.com>
2024-07-21 22:38:07 +0530

    almost completed

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ type Todo struct {
 
 var Todos []Todo = []Todo{
 	{Id: generateId(), Title: "Buy groceries", Description: "Milk, bread, eggs", IsCompleted: false},
-	{Id: generateId(), Title: "Finish report", Description: "Due tomorrow", IsCompleted: false},
+	{Id: generateId() + 1, Title: "Finish report", Description: "Due tomorrow", IsCompleted: false},
 }
 
 func generateId() int {
@@ -30,14 +30,15 @@ func main() {
 	http.HandleFunc("/", HealthCheck)
 	http.HandleFunc("/all-todos", AllTodos)
 	http.HandleFunc("/add-todo", AddTodo)
-	http.HandleFunc("/get-todo/{id}", GetTodoById)
-	http.HandleFunc("/delete-todo/{id}", DeleteTodo)
-	http.HandleFunc("/update-todo/{id}", UpdateTodo)
+	http.HandleFunc("/get-todo/", GetTodoById)
+	http.HandleFunc("/delete-todo/", DeleteTodo)
+	http.HandleFunc("/update-todo/", UpdateTodo)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
 
 // HealthCheck checks the health of the server
 func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("PATH::", r.URL.Path)
 	if r.Method != http.MethodGet {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
@@ -56,11 +57,12 @@ func HealthCheck(w http.ResponseWriter, r *http.Request) {
 
 // AllTodos returns all todos
 func AllTodos(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("PATH::", r.URL.Path)
 	if r.Method != http.MethodGet {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	if r.URL.Path != "/all-todos" {
+	if !strings.HasPrefix(r.URL.Path, "/all-todos") {
 		http.NotFound(w, r)
 		return
 	}
@@ -72,17 +74,51 @@ func AllTodos(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// AddTodo adds a new todo
+func AddTodo(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("PATH::", r.URL.Path)
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	if !strings.HasPrefix(r.URL.Path, "/add-todo") {
+		http.NotFound(w, r)
+		return
+	}
+	var newTodo Todo
+	err := json.NewDecoder(r.Body).Decode(&newTodo)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+	newTodo.Id = generateId()
+	Todos = append(Todos, newTodo)
+	w.WriteHeader(http.StatusCreated)
+	err = json.NewEncoder(w).Encode("Todo Added Successfully")
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+}
+
+// GetTodoById returns a todo by its ID
 func GetTodoById(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("PATH::", r.URL.Path)
 	if r.Method != http.MethodGet {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	if r.URL.Path != "/get-todo/"+strings.Split(r.URL.Path, "/")[2] {
+	if !strings.HasPrefix(r.URL.Path, "/get-todo/") {
 		http.NotFound(w, r)
 		return
 	}
-	pathSegment := strings.Split(r.URL.Path, "/")
-	todoId, err := strconv.Atoi(pathSegment[2])
+	idStr := strings.TrimPrefix(r.URL.Path, "/get-todo/")
+	if idStr == "" {
+		http.NotFound(w, r)
+		return
+	}
+	todoId, err := strconv.Atoi(idStr)
 	if err != nil {
 		http.Error(w, "Invalid todo ID", http.StatusBadRequest)
 		return
@@ -90,7 +126,7 @@ func GetTodoById(w http.ResponseWriter, r *http.Request) {
 	for index, todo := range Todos {
 		if todo.Id == todoId {
 			todo := Todos[index]
-			w.WriteHeader(http.StatusFound)
+			w.WriteHeader(http.StatusOK)
 			err := json.NewEncoder(w).Encode(todo)
 			if err != nil {
 				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
@@ -107,47 +143,28 @@ func GetTodoById(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func AddTodo(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	if r.URL.Path != "/add-todo" {
-		http.NotFound(w, r)
-		return
-	}
-	var newTodo Todo
-	err := json.NewDecoder(r.Body).Decode(&newTodo)
-	if err != nil {
-		http.Error(w, "Invalid request body", http.StatusBadRequest)
-		return
-	}
-	newTodo.Id = generateId()
-	Todos = append(Todos, newTodo)
-	w.WriteHeader(http.StatusCreated)
-	err = json.NewEncoder(w).Encode("Todo Added Successfully")
-	if err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-}
-
+// DeleteTodo deletes a todo by its ID
 func DeleteTodo(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("PATH::", r.URL.Path)
 	if r.Method != http.MethodDelete {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	if r.URL.Path != "/delete-todo/"+strings.Split(r.URL.Path, "/")[2] {
-		fmt.Println(r.URL.Path)
+	if !strings.HasPrefix(r.URL.Path, "/delete-todo/") {
 		http.NotFound(w, r)
 		return
 	}
-	pathSegment := strings.Split(r.URL.Path, "/")
-	todoId, err := strconv.Atoi(pathSegment[2])
+	idStr := strings.TrimPrefix(r.URL.Path, "/delete-todo/")
+	if idStr == "" {
+		http.NotFound(w, r)
+		return
+	}
+	todoId, err := strconv.Atoi(idStr)
 	if err != nil {
 		http.Error(w, "Invalid todo ID", http.StatusBadRequest)
 		return
 	}
+
 	for index, todo := range Todos {
 		if todo.Id == todoId {
 			Todos = append(Todos[:index], Todos[index+1:]...)
@@ -168,32 +185,42 @@ func DeleteTodo(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// UpdateTodo data -> is_completed true | false
+type UpdateTodoRequest struct {
+	IsCompleted bool `json:"is_completed"`
+}
+
+// UpdateTodo updates a todo by its ID
 func UpdateTodo(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("PATH::", r.URL.Path)
 	if r.Method != http.MethodPut {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	if r.URL.Path != "/update-todo/"+strings.Split(r.URL.Path, "/")[2] {
-		fmt.Println(r.URL.Path)
+
+	if !strings.HasPrefix(r.URL.Path, "/update-todo/") {
 		http.NotFound(w, r)
 		return
 	}
-
-	pathSegment := strings.Split(r.URL.Path, "/")
-	todoId, err := strconv.Atoi(pathSegment[2])
+	idStr := strings.TrimPrefix(r.URL.Path, "/update-todo/")
+	if idStr == "" {
+		http.NotFound(w, r)
+		return
+	}
+	todoId, err := strconv.Atoi(idStr)
 	if err != nil {
 		http.Error(w, "Invalid todo ID", http.StatusBadRequest)
 		return
 	}
-	var updatedTodo Todo
-	err = json.NewDecoder(r.Body).Decode(&updatedTodo)
+	var updateData UpdateTodoRequest
+	err = json.NewDecoder(r.Body).Decode(&updateData)
 	if err != nil {
-		http.Error(w, "Please provide updated todo data", http.StatusBadRequest)
+		http.Error(w, "Please provide enough data to update todo", http.StatusBadRequest)
 		return
 	}
 	for index, todo := range Todos {
 		if todo.Id == todoId {
-			Todos[index] = updatedTodo
+			Todos[index].IsCompleted = updateData.IsCompleted
 			w.WriteHeader(http.StatusOK)
 			err := json.NewEncoder(w).Encode("Todo Updated successfully")
 			if err != nil {
